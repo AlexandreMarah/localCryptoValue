a748b9d14041deed3bb25c22fe496ab819e1cc0f
AlexandreMarah <alexandre.marah@gmail.com>
2022-03-06 11:14:32 +0100

    Init project
    Initialize project from Spring Initializr

---
+++ src/main/java/com/alexandre/marah/localCryptoValue/LocalCryptoValueApplication.java
@@ -0,0 +1,13 @@
+package com.alexandre.marah.localCryptoValue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocalCryptoValueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocalCryptoValueApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alexandre/marah/localCryptoValue/LocalCryptoValueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alexandre.marah.localCryptoValue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocalCryptoValueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
